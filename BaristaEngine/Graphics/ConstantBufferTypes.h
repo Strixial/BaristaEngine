792356ac72e8cce5104f016ada1d67a4e7d8ab1d
Strixial <ostaniec1@protonmail.com>
2023-11-06 16:31:57 +0000

    remove everythign from directx branch lmao

--- BaristaEngine/Graphics/ConstantBufferTypes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <DirectXMath.h>
-
-struct CB_VS_vertexshader {
-	DirectX::XMFLOAT4X4 mat;
-};
--- BaristaEngine/StringConverter.cpp
+++
@@ -1,6 +0,0 @@
-#include "StringConverter.h"
-
-std::wstring StringConverter::StringToWide(std::string str) {
-	std::wstring wideString(str.begin(), str.end());
-	return wideString;
-}
